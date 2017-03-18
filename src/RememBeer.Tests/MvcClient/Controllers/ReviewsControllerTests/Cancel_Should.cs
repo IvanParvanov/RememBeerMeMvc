84e62582c1b86abce6f1fb6e341cde2fa1e1a776
J0hnyBG <ivan.parvanov1@gmail.com>
2017-03-18 19:22:40 +0200

    cancel unit tests & pagination icons

---
+++ src/RememBeer.Tests/MvcClient/Controllers/ReviewsControllerTests/Cancel_Should.cs
@@ -0,0 +1,116 @@
+﻿using System.Web.Mvc;
+
+using AutoMapper;
+
+using Moq;
+
+using Ninject;
+
+using NUnit.Framework;
+
+using RememBeer.Models.Contracts;
+using RememBeer.MvcClient.Controllers;
+using RememBeer.MvcClient.Filters;
+using RememBeer.MvcClient.Models.Reviews;
+using RememBeer.Services.Contracts;
+using RememBeer.Tests.MvcClient.Controllers.Ninject;
+using RememBeer.Tests.Utils;
+
+namespace RememBeer.Tests.MvcClient.Controllers.ReviewsControllerTests
+{
+    public class Cancel_Should : ReviewsControllerTestBase
+    {
+        [Test]
+        public void HaveAjaxOnlyAttribute()
+        {
+            // Act
+            var sut = this.Kernel.Get<ReviewsController>();
+            var hasAttribute = AttributeTester.MethodHasAttribute(() => sut.Cancel(default(int)), typeof(AjaxOnlyAttribute));
+
+            // Assert
+            Assert.IsTrue(hasAttribute);
+        }
+
+        [TestCase(1)]
+        [TestCase(49979687)]
+        [TestCase(314606869)]
+        public void Call_ReviewServiceGetByIdMethodOnceWithCorrectParams(int expectedId)
+        {
+            // Arrange
+            var sut = this.Kernel.Get<ReviewsController>();
+            var reviewService = this.Kernel.GetMock<IBeerReviewService>();
+            var viewModel = new SingleReviewViewModel();
+            var mapper = this.Kernel.GetMock<IMapper>();
+            mapper.Setup(m => m.Map<IBeerReview, SingleReviewViewModel>(It.IsAny<IBeerReview>()))
+                  .Returns(viewModel);
+
+            // Act
+            sut.Cancel(expectedId);
+
+            // Assert
+            reviewService.Verify(s => s.GetById(expectedId), Times.Once);
+        }
+
+        [Test]
+        public void Call_IMapperMapMethodOnceWithCorrectParams_WhenReviewIsFound()
+        {
+            // Arrange
+            var sut = this.Kernel.Get<ReviewsController>();
+            var expectedBeerReview = new Mock<IBeerReview>();
+            var reviewService = this.Kernel.GetMock<IBeerReviewService>();
+            reviewService.Setup(s => s.GetById(It.IsAny<int>()))
+                         .Returns(expectedBeerReview.Object);
+            var viewModel = new SingleReviewViewModel();
+            var mapper = this.Kernel.GetMock<IMapper>();
+            mapper.Setup(m => m.Map<IBeerReview, SingleReviewViewModel>(It.IsAny<IBeerReview>()))
+                  .Returns(viewModel);
+            // Act
+            sut.Cancel(It.IsAny<int>());
+
+            // Assert
+            mapper.Verify(m => m.Map<IBeerReview, SingleReviewViewModel>(expectedBeerReview.Object), Times.Once);
+        }
+
+        [Test]
+        public void Return_CorrectViewResult()
+        {
+            // Arrange
+            var sut = this.Kernel.Get<ReviewsController>();
+            var beerReview = new Mock<IBeerReview>();
+            var expectedViewModel = new SingleReviewViewModel();
+            var reviewService = this.Kernel.GetMock<IBeerReviewService>();
+            reviewService.Setup(s => s.GetById(It.IsAny<int>()))
+                         .Returns(beerReview.Object);
+            var mapper = this.Kernel.GetMock<IMapper>();
+            mapper.Setup(m => m.Map<IBeerReview, SingleReviewViewModel>(It.IsAny<IBeerReview>()))
+                  .Returns(expectedViewModel);
+            // Act
+            var result = sut.Cancel(It.IsAny<int>()) as PartialViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            var actualViewModel = result.Model as SingleReviewViewModel;
+            Assert.AreSame(expectedViewModel, actualViewModel);
+        }
+
+        [Test]
+        public void Set_ModelIsEditPropertyToTrue()
+        {
+            // Arrange
+            var sut = this.Kernel.Get<ReviewsController>();
+            var beerReview = new Mock<IBeerReview>();
+            var expectedViewModel = new SingleReviewViewModel();
+            var reviewService = this.Kernel.GetMock<IBeerReviewService>();
+            reviewService.Setup(s => s.GetById(It.IsAny<int>()))
+                         .Returns(beerReview.Object);
+            var mapper = this.Kernel.GetMock<IMapper>();
+            mapper.Setup(m => m.Map<IBeerReview, SingleReviewViewModel>(It.IsAny<IBeerReview>()))
+                  .Returns(expectedViewModel);
+            // Act
+            sut.Cancel(It.IsAny<int>());
+
+            // Assert
+            Assert.IsTrue(expectedViewModel.IsEdit);
+        }
+    }
+}
