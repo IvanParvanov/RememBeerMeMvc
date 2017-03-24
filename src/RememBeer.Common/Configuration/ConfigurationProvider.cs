f8bb5b2342d21680b058d39cc733cf47ede55f8e
J0hnyBG <ivan.parvanov1@gmail.com>
2017-03-24 14:06:19 +0200

    config provider refactoring

--- src/RememBeer.Common/Configuration/ConfigurationProvider.cs
+++ src/RememBeer.Common/Configuration/ConfigurationProvider.cs
@@ -7,215 +7,65 @@ namespace RememBeer.Common.Configuration
 {
     public class ConfigurationProvider : IConfigurationProvider
     {
-        public bool UserNamesAllowOnlyAlphanumeric
+        public bool UserNamesAllowOnlyAlphanumeric => ParseBoolConfig(nameof(this.UserNamesAllowOnlyAlphanumeric));
+
+        public bool RequireUniqueEmail => ParseBoolConfig(nameof(this.RequireUniqueEmail));
+
+        public bool PasswordRequireNonLetterOrDigit => ParseBoolConfig(nameof(this.PasswordRequireNonLetterOrDigit));
+
+        public bool PasswordRequireDigit => ParseBoolConfig(nameof(this.PasswordRequireDigit));
+
+        public bool PasswordRequireLowercase => ParseBoolConfig(nameof(this.PasswordRequireLowercase));
+
+        public bool PasswordRequireUppercase => ParseBoolConfig(nameof(this.PasswordRequireUppercase));
+
+        public bool UserLockoutEnabledByDefault => ParseBoolConfig(nameof(this.UserLockoutEnabledByDefault));
+
+        public int PasswordMinLength => ParseIntConfig(nameof(this.PasswordMinLength));
+
+        public int DefaultAccountLockoutTimeSpan => ParseIntConfig(nameof(this.DefaultAccountLockoutTimeSpan));
+
+        public int MaxFailedAccessAttemptsBeforeLockout => ParseIntConfig(nameof(this.MaxFailedAccessAttemptsBeforeLockout));
+
+        public string ImageUploadName => GetConfigValue(nameof(this.ImageUploadName));
+
+        public string ImageUploadApiKey => GetConfigValue(nameof(this.ImageUploadApiKey));
+
+        public string ImageUploadApiSecret => GetConfigValue(nameof(this.ImageUploadApiSecret));
+
+        public string MailSenderEmailAddress => GetConfigValue(nameof(this.MailSenderEmailAddress));
+
+        public string MailUsername => GetConfigValue(nameof(this.MailUsername));
+
+        public string MailPassword => GetConfigValue(nameof(this.MailPassword));
+
+        private static bool ParseBoolConfig(string settingName)
         {
-            get
+            try
             {
-                try
-                {
-                    var val = TryGetValue(nameof(this.UserNamesAllowOnlyAlphanumeric));
-                    return bool.Parse(val);
-                }
-                catch (FormatException)
-                {
-                    throw new InvalidConfigurationOptionException(nameof(this.UserNamesAllowOnlyAlphanumeric));
-                }
+                var val = GetConfigValue(settingName);
+                return bool.Parse(val);
+            }
+            catch (FormatException)
+            {
+                throw new InvalidConfigurationOptionException(settingName);
             }
         }
 
-        public bool RequireUniqueEmail
+        private static int ParseIntConfig(string settingName)
         {
-            get
+            try
             {
-                try
-                {
-                    var val = TryGetValue(nameof(this.RequireUniqueEmail));
-                    return bool.Parse(val);
-                }
-                catch (FormatException)
-                {
-                    throw new InvalidConfigurationOptionException(nameof(this.RequireUniqueEmail));
-                }
+                var val = GetConfigValue(settingName);
+                return int.Parse(val);
+            }
+            catch (FormatException)
+            {
+                throw new InvalidConfigurationOptionException(settingName);
             }
         }
 
-        public int PasswordMinLength
-        {
-            get
-            {
-                try
-                {
-                    var val = TryGetValue(nameof(this.PasswordMinLength));
-                    return int.Parse(val);
-                }
-                catch (FormatException)
-                {
-                    throw new InvalidConfigurationOptionException(nameof(this.PasswordMinLength));
-                }
-            }
-        }
-
-        public bool PasswordRequireNonLetterOrDigit
-        {
-            get
-            {
-                try
-                {
-                    var val = TryGetValue(nameof(this.PasswordRequireNonLetterOrDigit));
-                    return bool.Parse(val);
-                }
-                catch (FormatException)
-                {
-                    throw new InvalidConfigurationOptionException(nameof(this.PasswordRequireNonLetterOrDigit));
-                }
-            }
-        }
-
-        public bool PasswordRequireDigit
-        {
-            get
-            {
-                try
-                {
-                    var val = TryGetValue(nameof(this.PasswordRequireDigit));
-                    return bool.Parse(val);
-                }
-                catch (FormatException)
-                {
-                    throw new InvalidConfigurationOptionException(nameof(this.PasswordRequireDigit));
-                }
-            }
-        }
-
-        public bool PasswordRequireLowercase
-        {
-            get
-            {
-                try
-                {
-                    var val = TryGetValue(nameof(this.PasswordRequireLowercase));
-                    return bool.Parse(val);
-                }
-                catch (FormatException)
-                {
-                    throw new InvalidConfigurationOptionException(nameof(this.PasswordRequireLowercase));
-                }
-            }
-        }
-
-        public bool PasswordRequireUppercase
-        {
-            get
-            {
-                try
-                {
-                    var val = TryGetValue(nameof(this.PasswordRequireUppercase));
-                    return bool.Parse(val);
-                }
-                catch (FormatException)
-                {
-                    throw new InvalidConfigurationOptionException(nameof(this.PasswordRequireUppercase));
-                }
-            }
-        }
-
-        public bool UserLockoutEnabledByDefault
-        {
-            get
-            {
-                try
-                {
-                    var val = TryGetValue(nameof(this.UserLockoutEnabledByDefault));
-                    return bool.Parse(val);
-                }
-                catch (FormatException)
-                {
-                    throw new InvalidConfigurationOptionException(nameof(this.UserLockoutEnabledByDefault));
-                }
-            }
-        }
-
-        public int DefaultAccountLockoutTimeSpan
-        {
-            get
-            {
-                try
-                {
-                    var val = TryGetValue(nameof(this.DefaultAccountLockoutTimeSpan));
-                    return int.Parse(val);
-                }
-                catch (FormatException)
-                {
-                    throw new InvalidConfigurationOptionException(nameof(this.DefaultAccountLockoutTimeSpan));
-                }
-            }
-        }
-
-        public int MaxFailedAccessAttemptsBeforeLockout
-        {
-            get
-            {
-                try
-                {
-                    var val = TryGetValue(nameof(this.MaxFailedAccessAttemptsBeforeLockout));
-                    return int.Parse(val);
-                }
-                catch (FormatException)
-                {
-                    throw new InvalidConfigurationOptionException(nameof(this.MaxFailedAccessAttemptsBeforeLockout));
-                }
-            }
-        }
-
-        public string ImageUploadName
-        {
-            get
-            {
-                return TryGetValue(nameof(this.ImageUploadName));
-            }
-        }
-
-        public string ImageUploadApiKey
-        {
-            get
-            {
-                return TryGetValue(nameof(this.ImageUploadApiKey));
-            }
-        }
-
-        public string ImageUploadApiSecret
-        {
-            get
-            {
-                return TryGetValue(nameof(this.ImageUploadApiSecret));
-            }
-        }
-
-        public string MailSenderEmailAddress
-        {
-            get
-            {
-                return TryGetValue(nameof(this.MailSenderEmailAddress));
-            }
-        }
-
-        public string MailUsername
-        {
-            get
-            {
-                return TryGetValue(nameof(this.MailUsername));
-            }
-        }
-
-        public string MailPassword
-        {
-            get
-            {
-                return TryGetValue(nameof(this.MailPassword));
-            }
-        }
-
-        private static string TryGetValue(string settingName)
+        private static string GetConfigValue(string settingName)
         {
             var value = ConfigurationManager.AppSettings[settingName];
             if (value == null)
